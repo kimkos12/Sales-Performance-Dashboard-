c95cab7b2a25323c4e3d99f624969e174d641235
kimutai victor Kosgei <weldonruto41@gmail.com>
2026-09-14 08:51:51 +0300

    Update sales_analysis.sql

--- sql/sales_analysis.sql
+++ sql/sales_analysis.sql
@@ -1,1 +1,37 @@
+-- Sales Performance Dashboard
+-- SQL Business Analysis
 
+-- 1. Total Revenue
+SELECT
+    SUM(Revenue) AS Total_Revenue
+FROM SalesData;
+
+
+-- 2. Total Units Sold
+SELECT
+    SUM(Units_Sold) AS Total_Units_Sold
+FROM SalesData;
+
+
+-- 3. Total Net Profit
+SELECT
+    SUM(Net_Profit) AS Total_Net_Profit
+FROM SalesData;
+
+
+-- 4. Revenue by Region
+SELECT
+    Region,
+    SUM(Revenue) AS Total_Revenue
+FROM SalesData
+GROUP BY Region
+ORDER BY Total_Revenue DESC;
+
+
+-- 5. Net Profit by Product
+SELECT
+    Product,
+    SUM(Net_Profit) AS Total_Net_Profit
+FROM SalesData
+GROUP BY Product
+ORDER BY Total_Net_Profit DESC;
